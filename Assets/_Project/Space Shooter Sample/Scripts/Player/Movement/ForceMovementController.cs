7c98384e96b0b6096c041de33149411baffa2b32
Jolo Gutierrez <mariano.jose.gutierrez.iii@dlsl.edu.ph>
2024-09-06 20:01:23 +0800

    adjusted the ship controls

--- Assets/_Project/Space Shooter Sample/Scripts/Player/Movement/ForceMovementController.cs
+++ Assets/_Project/Space Shooter Sample/Scripts/Player/Movement/ForceMovementController.cs
@@ -4,6 +4,7 @@ public class ForceMovementController : MovementController
 {
     [SerializeField] private Rigidbody2D _rigidbody;
     [SerializeField] private float _maxSpeed;
+    [SerializeField] private float _rotationSpeed;
 
     protected override Vector2 HandleInputDirection()
     {
@@ -12,13 +13,21 @@ public class ForceMovementController : MovementController
 
     protected override void Move(Vector2 direction, float deltaTime)
     {
-        _rigidbody.AddForce(deltaTime * Speed * direction);
+        var thrust = transform.up * direction.y;
+        _rigidbody.AddForce(deltaTime * Speed * thrust);
+        _rigidbody.MoveRotation(_rigidbody.rotation - direction.x * _rotationSpeed * deltaTime);
 
+        // Limit the speed of our ship
         if (_rigidbody.velocity.magnitude > _maxSpeed)
         {
             _rigidbody.velocity = _rigidbody.velocity.normalized * _maxSpeed;
         }
 
+    }
+    protected override void Update()
+    {
+        // base.Update();
+        Direction = HandleInputDirection().normalized;
     }
     private void FixedUpdate()
     {
